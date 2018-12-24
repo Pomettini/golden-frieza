bdf6e06d997d251c42ff426642789c7365f95161
Giorgio Pomettini <giorgio.pomettini@gmail.com>
2018-12-24 02:53:40 +0100

    Added GUI library

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,13 @@
 extern crate csv;
 extern crate golden_frieza;
+extern crate iui;
 
 use golden_frieza::*;
+use iui::controls::{Button, MultilineEntry, VerticalBox};
+use iui::prelude::*;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
 use std::path::Path;
 
 fn main() {
@@ -12,4 +18,13 @@ fn main() {
     for key in colors.dictionary.keys() {
         println!("{}", key);
     }
+
+    // Initialize the UI
+    let ui = UI::init().unwrap();
+
+    // Set up the application's layout
+    let mut window = Window::new(&ui, "Golden Frieza", 640, 480, WindowType::NoMenubar);
+    window.show(&ui);
+
+    ui.main();
 }
